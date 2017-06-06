7a822b2f9fd90f439cee94054d80a50a2a8757f0
Samuel Bock <nonvirtualthunk@gmail.com>
2017-06-06 07:51:33 -0700

    Rich text layout, allow for colors and images in text rendering (at the lowest level, still needs basically all of the implementation above it)

--- engine/src/main/scala/arx/graphics/helpers/TextLayouter.scala
+++ engine/src/main/scala/arx/graphics/helpers/TextLayouter.scala
@@ -11,28 +11,83 @@ package arx.graphics.helpers
 import arx.Prelude._
 import arx.application.Noto
 import arx.core.math.Rectf
-import arx.core.vec.Cardinals
-import arx.core.vec.ReadVec2f
-import arx.core.vec.Vec2f
+import arx.core.vec.{Cardinals, ReadVec2f, ReadVec4f, Vec2f}
+import arx.graphics.TToImage
 import arx.graphics.helpers.TTextLayouter.LayoutParameters
 import arx.graphics.text.TBitmappedFont
 
 
+sealed abstract class RichTextSection {
+	def symbolAtIndex(i : Int) : Any
+	def symbolCount : Int
+	def colorAtIndex(i : Int) : ReadVec4f
+}
+case class TextSection(text : String, color : ReadVec4f = Color.Black) extends RichTextSection {
+	override def symbolAtIndex(i: Int): Any = text(i)
+	override def symbolCount: Int = text.length
+	override def colorAtIndex(i: Int): ReadVec4f = color
+}
+case class ImageSection(image : TToImage, scale : Float, color : ReadVec4f = Color.Black) extends RichTextSection {
+	override def symbolAtIndex(i: Int): Any = i match {
+		case 0 => image.image
+		case _ => Noto.severeError("Out of bounds access to image rich-text section"); '~'
+	}
+	override def symbolCount: Int = 1
+	override def colorAtIndex(i: Int): ReadVec4f = color
+}
+
+case class RichText (sections : List[RichTextSection]) {
+	def symbolCount = sections.isum(s => s.symbolCount)
+	protected def getFromIndex[T](target : Int, func : (TextSection, Int) => T) = {
+		var tmpS = sections
+		var i = target
+		while (tmpS.nonEmpty && i >= tmpS.head.symbolCount) {
+			i -= tmpS.head.symbolCount
+			tmpS = tmpS.tail
+		}
+		if (tmpS.nonEmpty) {
+			func(tmpS(i))
+		} else {
+			throw new IndexOutOfBoundsException("Attempted to access past the end of a rich text")
+		}
+	}
+	def symbolAtIndex(target : Int) = {
+		getFromIndex(target, (s, i) => s.symbolAtIndex(i))
+	}
+	def colorAtIndex(target : Int) = {
+		getFromIndex(target, (s,i) => s.colorAtIndex(i))
+	}
+}
+
+
 class TextLayouter extends TTextLayouter {
 	def layOutText (text : String, font : TBitmappedFont, fontSize : Float, area : Rectf, spacingMultiple : Float = 1.0f, minSpacing : Float = 0.0f, textAlignment : Int = Cardinals.Left ) : TextLayoutResult = {
+		layOutText(RichText(TextSection(text) :: Nil), font, fontSize, area, spacingMultiple, minSpacing, textAlignment)
+	}
+
+	def layOutText (richText : RichText, font : TBitmappedFont, fontSize : Float, area : Rectf, spacingMultiple : Float = 1.0f, minSpacing : Float = 0.0f, textAlignment : Int = Cardinals.Left ) : TextLayoutResult = {
 		var x = 0.0f
 		var y = 0.0f
 		var maxX = 0.0f
 		var maxY = 0.0f
 
-		var pointList: List[Vec2f] = Nil
+		var maxSpacingThisLine = 0.0f
+
+		var points: Vector[Vec2f] = Vector()
+		var widths : Vector[Float] = Vector()
+
+		def jumpToNextLine() = {
+			y += lineSpacing(font, fontSize).max(maxSpacingThisLine)
+			x = 0.0f
+			maxY = math.max(maxY, y)
+			maxSpacingThisLine = 0.0f
+		}
 
 		def renderWord(word: String) {
 			val width = stringWidth(word, font, fontSize)
 			//If this is the only word on the line and it's longer than the line, no point skipping down
 			if (x + width > area.w && x > 0.00001f) {
-				y += lineSpacing(font,fontSize); x = 0.0f
-				maxY = math.max(maxY,y)
+				jumpToNextLine()
 			}
 			var wordIndex = 0
 			while ( wordIndex < word.length() ) {
@@ -40,12 +95,11 @@ class TextLayouter extends TTextLayouter {
 				val cw = charWidth(c, font, fontSize)
 				//If we're mid-word when we hit then end, skip down
 				if ( x + cw > area.w ) {
-					x = 0.0f
-					y += lineSpacing(font,fontSize); x = 0.0f
-					maxY = math.max(maxY,y)
+					jumpToNextLine()
 				}
 
-				pointList ::= Vec2f(area.x + x,area.y + y)
+				points :+= Vec2f(area.x + x,area.y + y)
+				widths :+= cw
 
 				x += cw * spacingMultiple + minSpacing
 				maxX = math.max(maxX,x)
@@ -54,41 +108,64 @@ class TextLayouter extends TTextLayouter {
 			}
 		}
 
-		val words = text.split(TextLayouter.whitespaceArray)
-		var i = 0
-		for (word <- words) {
-			if (!word.isEmpty) {
-				renderWord(word)
-				i += word.length
-			}
-			while (i < text.length && TextLayouter.whitespaceSet.contains(text(i))) {
-				text(i) match {
-					case ' ' =>
-						pointList ::= Vec2f(area.x + x,area.y + y)
-						x += spaceSize(fontSize) * spacingMultiple
-					case '\n' => {
-						x = 0.0f; y += lineSpacing(font,fontSize)
-						maxY = math.max(maxY, y)
-						pointList ::= Vec2f(area.x + x,area.y + y)
+		for (section <- richText.sections) {
+			section match {
+				case TextSection(text,_) =>
+					val words = text.split(TextLayouter.whitespaceArray)
+					var i = 0
+					for (word <- words) {
+						if (!word.isEmpty) {
+							renderWord(word)
+							i += word.length
+						}
+						while (i < text.length && TextLayouter.whitespaceSet.contains(text(i))) {
+							text(i) match {
+								case ' ' =>
+									val sw = spaceSize(fontSize) * spacingMultiple
+									points :+= Vec2f(area.x + x,area.y + y)
+									widths :+= sw
+									x += sw
+								case '\n' => {
+									jumpToNextLine()
+									points :+= Vec2f(area.x + x,area.y + y)
+									widths :+= 0.0f
+								}
+								case '\t' =>
+									val tw = tabSize(fontSize) * spacingMultiple
+									points :+= Vec2f(area.x + x,area.y + y)
+									widths :+= tw
+									x += tw
+								case _ => Noto.error("AAAAH")
+							}
+							maxX = math.max(maxX,x)
+							i += 1
+						}
 					}
-					case '\t' =>
-						pointList ::= Vec2f(area.x + x,area.y + y)
-						x += tabSize(fontSize) * spacingMultiple
-					case _ => Noto.error("AAAAH")
-				}
-				maxX = math.max(maxX,x)
-				i += 1
+				case ImageSection(image, scale, _) =>
+					val width = image.width * scale
+					//If this is the only thing on the line and it's longer than the line, no point skipping down, otherwise
+					if (x + width > area.w && x > 0.00001f) {
+						jumpToNextLine()
+					}
+					maxSpacingThisLine = image.height * scale // ensure that we don't collide with this image on the next line
+
+					points :+= Vec2f(area.x + x,area.y + y)
+					widths :+= width
+
+					x += width + minSpacing
+					maxX = math.max(maxX,x)
 			}
 		}
 
-		val points = pointList.reverse
+
 		if ( points.nonEmpty && textAlignment == Cardinals.Center ) {
+			val lastSymbolWidth = widths.last
 			val minX = points.fmin(_.x - area.x)
 			val maxX = points.fmax(_.x - area.x)
 			val minY = points.fmin(_.y - area.y)
 			val maxY = points.fmax(_.y - area.y)
-			val ox = (area.width - (maxX - minX + charWidth(text.last,font,fontSize))) * 0.5f
-			val oy = (area.height - (maxY - minY + charHeight(text.last,font,fontSize))) * 0.5f
+			val ox = (area.width - (maxX - minX + lastSymbolWidth)) * 0.5f
+			val oy = (area.height - (maxY - minY + lastSymbolWidth)) * 0.5f
 			for (p <- points) { p.x += ox; p.y += oy }
 		} else if ( points.nonEmpty && textAlignment == Cardinals.Right ) {
 			var maxXForLine = 0.0f
@@ -103,13 +180,17 @@ class TextLayouter extends TTextLayouter {
 				maxXForLine = 0.0f
 			}
 
-			for ((p,t) <- points.zip(text)) {
+			var i = 0
+			while (i < points.size) {
+				val p = points(i)
+				val w = widths(i)
 				if (p.y != lastY) {
 					adjustLine()
 				}
 				pointsOnLine :+= p
-				maxXForLine = math.max(p.x + charWidth(t,font,fontSize),maxXForLine)
+				maxXForLine = math.max(p.x + w,maxXForLine)
 				lastY = p.y
+				i += 1
 			}
 			adjustLine()
 		}
@@ -170,4 +251,4 @@ object TTextLayouter {
 	case class LayoutParameters ( text : String , font : TBitmappedFont , fontSize : Float , area : Rectf , spacingMultiple : Float, minSpacing : Float, textAlignment : Int )
 }
 
-case class TextLayoutResult ( points : List[ReadVec2f] , dimensions : ReadVec2f )
+case class TextLayoutResult ( points : Vector[ReadVec2f] , dimensions : ReadVec2f )
